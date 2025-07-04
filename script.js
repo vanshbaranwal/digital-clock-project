5af4cfae8849cf0bc5dc04aaaa4b37195cbaa20e
vanshbaranwal <vanshveerbaranwal@gmail.com>
2025-07-05 03:38:53 +0530

    created a digital clock

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert("vanshbaranwal")
+function updateClock(){
+    const timeElement = document.getElementById("time");
+    const dateElement = document.getElementById("date");
+
+    const now = new Date();
+    const hours = now.getHours() % 12 || 12;
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const ampm = now.getHours() <= 12 ? "AM" : "PM";
+
+    const options = {
+        weekday : "long",
+        year : "numeric",
+        month : "long",
+        day : "numeric",
+    };
+    
+    const dateString = now.toLocaleDateString(undefined, options)
+
+    timeElement.textContent = `${hours}:${minutes}:${seconds} ${ampm}`
+    dateElement.textContent = `${dateString}`
+}
+
+
+setInterval(updateClock, 1000);
+updateClock();
